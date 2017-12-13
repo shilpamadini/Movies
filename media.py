db875d437604fdaaaf4f8c9e98381a09ac8504c4
shilpamadini <shilpa.madini@gmail.com>
2017-12-13 12:35:07 -0800

    Final code for the definition of class Movie

--- media.py
+++ media.py
@@ -2,12 +2,15 @@ import webbrowser
 
 class Movie():
     """ This class provides a blueprint to store movies information"""
+    # Declare the class variable to store valid movie ratings
     VALID_RATINGS = ['G','PG','PG-13','R']
     def __init__(self,movie_title,story,image,trailor):
+        """ Constructor for the movie instances that initiate instance variable
+        movie_title,story,box image, youtube trailer video"""
         self.title = movie_title
         self.storyline = story
         self.poster_image_url = image
         self.trailer_youtube_url = trailor
 
-    def show_trailor(self):
-        webbrowser.open(self.youtube_url)
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)

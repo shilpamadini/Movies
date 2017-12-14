28df448700e4e61d6f98e484cdb71c179fdb5cef
shilpamadini <shilpa.madini@gmail.com>
2017-12-14 10:49:35 -0800

    Refactored the code to follow PEP8 standards

--- media.py
+++ media.py
@@ -1,16 +1,36 @@
 import webbrowser
 
+
 class Movie():
-    """ This class provides a blueprint to store movies information"""
+    """
+    This class provides a blueprint to store movies information
+    """
     # Declare the class variable to store valid movie ratings
-    VALID_RATINGS = ['G','PG','PG-13','R']
-    def __init__(self,movie_title,story,image,trailor):
-        """ Constructor for the movie instances that initiate instance variable
-        movie_title,story,box image, youtube trailer video"""
+    VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
+
+    def __init__(
+        self,
+        movie_title,
+        story,
+        image,
+        trailor
+    ):
+
+        """
+        Constructor for the movie instances that initiate instance variables
+        :param movie_title: string
+        :param story: string
+        :param image: string
+        :param trailor: string
+        """
+
         self.title = movie_title
         self.storyline = story
         self.poster_image_url = image
         self.trailer_youtube_url = trailor
-    # Function to that opens a web browser to show the youtube video
+
     def show_trailer(self):
+        """
+        Initialize instance for opening youtube video
+        """
         webbrowser.open(self.trailer_youtube_url)
